508aea219c05eb56c73cff51f1d116df566eb70c
ib1zza <dremast1337@gmail.com>
2022-11-15 19:29:56 +0300

    added font awesome

--- js/index.js
+++ js/index.js
@@ -1,11 +1,25 @@
-function ibg() {
+// import anime from "./anime";
 
-    let ibg = document.querySelectorAll(".ibg");
-    for (var i = 0; i < ibg.length; i++) {
-        if (ibg[i].querySelector('img')) {
-            ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
-        }
+function ibg() {
+  let ibg = document.querySelectorAll(".ibg");
+  for (var i = 0; i < ibg.length; i++) {
+    if (ibg[i].querySelector("img")) {
+      ibg[i].style.backgroundImage =
+        "url(" + ibg[i].querySelector("img").getAttribute("src") + ")";
     }
+  }
 }
 
 ibg();
+
+const reactLogoEl = document.getElementById("react");
+
+const animate = (el) => {
+  el.style.tran;
+};
+reactLogoEl.addEventListener("mouseenter", () => animate(reactLogoEl));
+
+anime({
+  targets: reactLogoEl,
+  translateX: 270
+});
